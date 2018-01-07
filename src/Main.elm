cd062abe5bc1c14388f16e502b595b7cee7c3101
Kolja Lampe <razzeee@gmail.com>
2018-01-08 00:48:46 +0100

    Build final character page

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Input.Number as Number
+import List.Extra exposing (..)
 
 main =
     Html.program { init = init, view = view, update = update, subscriptions = always Sub.none }
@@ -25,7 +26,8 @@ type Page
 type alias ListItem =
     { id : String,
     name : String,
-    value : Maybe Int}
+    value : Maybe Int,
+    masterItem : Bool}
 
 type alias Character =
     { name : String
@@ -41,11 +43,12 @@ type alias Character =
     , interactList : List  ListItem
     , actsList : List ListItem
     , unassignedPoints : String
+    , picture : String
     }
 
 init : (Model, Cmd Msg)
 init =
-    (Model (Character "" "" "" "" "" "" "" "" (Just 100) [(ListItem "knowledge" "Wissen" (Just 0))] [(ListItem"interact" "Interagieren" (Just 0))] [(ListItem "acts" "Handeln" (Just 0))] "500") PageBaseProperties, Cmd.none)
+    (Model (Character "" "" "" "" "" "" "" "" (Just 100) [(ListItem "knowledge" "Wissen" (Just 0) True)] [(ListItem"interact" "Interagieren" (Just 0) True)] [(ListItem "acts" "Handeln" (Just 0) True)] "500" "img/character.jpg") PageBaseProperties, Cmd.none)
 
 
 
@@ -326,4 +329,119 @@ pageSkillpoints model =
 characterSheet : Model -> Html Msg
 characterSheet model =
     div [ class "container" ]
-        []
+        [ 
+             div [ class "field" ]
+        [ label [ class "label has-text-centered" ]
+            [ text "Charakterbogen" ]
+        ]
+        , div [ class "columns"] [
+            div [ class "column" ] [
+                showReadonlyValue model.character.firstName "Vorname"
+                ,showReadonlyValue model.character.sex "Geschlecht"
+            ]
+            , div [ class "column" ] [
+                showReadonlyValue model.character.name "Name"
+                ,showReadonlyValue model.character.age "Alter"
+            ]
+            , div [ class "column" ] [
+                img [src model.character.picture] []
+            ]
+            , div [ class "column" ] [
+                showReadonlyValue model.character.stature "Statur"
+                , showReadonlyValue model.character.job "Beruf"
+            ]
+            , div [ class "column" ] [
+                showReadonlyValue model.character.religion "Religion"
+                , showReadonlyValue model.character.familyStatus "Familienstand"
+            ]
+        ]
+        , div [ class "columns"] [
+            div [ class "column" ] [
+            ]
+            , div [ class "column" ] [
+            ]
+            , div [ class "column" ] [
+                showReadonlyLifepoints model.character.lifePoints "Lebenspunkte"
+            ]
+            , div [ class "column" ] [
+            ]
+            , div [ class "column" ] [
+            ]
+        ]
+        , div [ class "columns"] [
+            div [ class "column" ] [
+                showReadonlyListItemValue (List.Extra.find (\value -> value.masterItem) model.character.actsList) "Handeln"
+            ]
+            , div [ class "column" ] [
+                showReadonlyListItemValue (List.Extra.find (\value -> value.masterItem) model.character.knowledgeList) "Wissen"
+            ]
+            , div [ class "column" ] [
+                showReadonlyListItemValue (List.Extra.find (\value -> value.masterItem) model.character.interactList) "Interagieren"
+            ]
+        ]
+        , div [ class "columns"] [
+            div [ class "column" ] []
+            , div [ class "column" ] []
+            , div [ class "column" ] []
+        ]
+        , (nextButton model PageSkillpoints CharacterSheet)
+        ]
+
+showReadonlyValue: String -> String -> Html Msg
+showReadonlyValue value title =
+    div [ class "field" ]
+        [ label [ class "label" ]
+            [ text title ]
+        , div [ class "control" ]
+            [ input [ class "input", type_ "text", disabled True, readonly True]
+                [ text value ]
+            ]
+        ]
+
+
+showReadonlyListItemValue listItem title =
+    case listItem of
+        Nothing ->
+            div [ class "field" ]
+                [ label [ class "label" ]
+                    [ text title ]
+                , div [ class "control" ]
+                    [ input [ class "input", type_ "text", disabled True, readonly True]
+                        [ text "" ]
+                    ]
+                ]
+        Just value ->
+            div [ class "field" ]
+                [ label [ class "label" ]
+                    [ text title ]
+                , div [ class "control" ]
+                    [ input [ class "input", type_ "text", disabled True, readonly True]
+                        [ text (convertMaybeIntToString value.value) ]
+                    ]
+                ]
+
+showReadonlyLifepoints: Maybe Int -> String -> Html Msg
+showReadonlyLifepoints value title =
+    div [ class "field" ]
+        [ label [ class "label" ]
+            [ text title ]
+        , div [ class "control" ]
+            [ Number.input
+                { onInput = ChangeLifepoints
+                , maxLength = Nothing
+                , maxValue = Just 100
+                , minValue = Just 0,
+                hasFocus = Just FocusChanged
+                }
+                [ class "input", disabled True, readonly True
+                ]
+                value
+            ]
+        ]
+    
+convertMaybeIntToString input =
+    case input of
+        Nothing ->
+        "0"
+        Just value ->
+        toString value
