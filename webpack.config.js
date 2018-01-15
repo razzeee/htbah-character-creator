4db75f10d98c27f8e37219a6ac6238d0ff0724c5
Kolja Lampe <razzeee@gmail.com>
2018-01-15 01:48:25 +0100

    Test this

--- webpack.config.js
+++ webpack.config.js
@@ -157,7 +157,7 @@ if (TARGET_ENV === 'production') {
         {
           test: /\.elm$/,
           exclude: [/elm-stuff/, /node_modules/],
-          use: ['elm-hot-loader', 'elm-webpack-loader?verbose=true&warn=true&pathToMake="'+ prefix + '/bin/elm-make'],
+          use: ['elm-hot-loader', 'elm-webpack-loader?verbose=true&warn=true&pathToMake=sysconfcpus/bin/sysconfcpus -n 2 elm-make'],
         },
       ],
     }
